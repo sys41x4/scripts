577a1476a802c55f0aad1b581375a6be84dfb467
sys41x4 <arijit-bhowmick@outlook.com>
2024-09-09 01:50:33 +0530

    Added postgres-client-16 installation script for debian-bookworm

---
+++ postgresql/16/debian-bookworm/client/postgres-client.install.sh
@@ -0,0 +1,13 @@
+# Import the repository signing key:
+sudo apt install -y curl ca-certificates lsb-release
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+
+# Create the repository configuration file:
+sudo sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Update the package lists:
+sudo apt update
+
+# Install postgresql-client-16
+sudo apt install -y postgresql-client-16
